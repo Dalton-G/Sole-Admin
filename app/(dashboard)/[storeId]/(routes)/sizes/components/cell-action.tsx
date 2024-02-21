87204ce68add752952e7f391a17ed256d533dbd2
Dalton Gan <study.daltongan@gmail.com>
2024-02-21 19:26:16 +0800

    feat: added ability to CRUD sizes

--- app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
@@ -20,11 +20,11 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 
-import { CategoryColumn } from "./columns";
+import { SizeColumn } from "./columns";
 import { AlertModal } from "@/components/modals/alert-modal";
 
 interface CellActionProps {
-    data: CategoryColumn;
+    data: SizeColumn;
 };
 
 export const CellAction: React.FC<CellActionProps> = ({
@@ -39,17 +39,17 @@ export const CellAction: React.FC<CellActionProps> = ({
 
     const onCopy = (id: string) => {
         navigator.clipboard.writeText(id);
-        toast.success("Category ID copied to clipboard!");
+        toast.success("Size ID copied to clipboard!");
     }
 
     const onDelete = async () => {
         try {
             setLoading(true);
-            await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
+            await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
             router.refresh();
-            toast.success("Category deleted!");
+            toast.success("Size deleted!");
         } catch (error) {
-            toast.error("Make sure you remove all product using this category first.");
+            toast.error("Make sure you remove all products using this size first.");
         } finally {
             setLoading(false);
             setOpen(false);
@@ -81,7 +81,7 @@ export const CellAction: React.FC<CellActionProps> = ({
                         <Copy className="mr-2 h-4 w-4" />
                         Copy ID
                     </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/categories/${data.id}`)}>
+                    <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}>
                         <Edit className="mr-2 h-4 w-4" />
                         Update
                     </DropdownMenuItem>
