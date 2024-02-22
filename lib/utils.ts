e5718e3cc15b6ac228f453df90b30dd34c79e8c5
Dalton Gan <study.daltongan@gmail.com>
2024-02-22 15:26:31 +0800

    feat: added products to dashboard and CRUD functionality

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatter = new Intl.NumberFormat("en-MY", {
+  style: "currency",
+  currency: "MYR",
+})
+
+// may change to en-US -> USD
